c456276bafaf95a63eb02ebf65d37b1655afea11
Saumza <saumyajeetbhowmick@gmail.com>
2026-06-16 02:22:45 +0530

    Enhance URL analytics functionality with Redis caching and error handling

--- backend/src/controller/analytics.controller.ts
+++ backend/src/controller/analytics.controller.ts
@@ -5,6 +5,8 @@ import { APIError } from "../utils/APIError.js";
 import type { Request, Response } from "express";
 import { LinkModel } from "../models/link.model.js";
 import type { ShortenedURLParams } from "../middleware/shortenedURLValidation.middleware.js";
+import { ShortenedUrlKey } from "../utils/RedisKey.js";
+import { redis } from "../db/redisConnect.js";
 
 
 const urlAnalytics = asyncHandler(async (req: Request<ShortenedURLParams>, res: Response) => {
@@ -15,17 +17,41 @@ const urlAnalytics = asyncHandler(async (req: Request<ShortenedURLParams>, res: 
         throw new APIError(400, "ShortenedURL not Received")
     }
 
-    const findURL = await LinkModel.findOne({
-        shortenedId
-    })
+    const urlKey = ShortenedUrlKey(shortenedId)
+    const user = await redis.hgetall(urlKey)
 
-    if (!findURL) {
-        throw new APIError(404, "URL Not Available")
+    if (Object.keys(user).length === 0) {
+
+        const findURL = await LinkModel.findOne({
+            shortenedId
+        }).lean()
+
+        if (!findURL) {
+            throw new APIError(404, "URL Not Available")
+        }
+
+        const redisHashFile: any = {}
+
+        for (const [key, value] of Object.entries(findURL)) {
+            if ((typeof value === "object" || Array.isArray(value)) && value !== null) {
+                redisHashFile[key] = JSON.stringify(value)
+            }
+            redisHashFile[key] = String(value)
+        }
+
+
+        await redis.hset(urlKey, findURL)
+        return res.status(201).json(new APIResponse(201, "Analytics Fetched Successfully", findURL.urlClicks))
     }
 
-    return res.status(201).json(new APIResponse(201, "Analytics Fetched Successfully", findURL))
+    if (!user.uploadedURL) {
+        console.log("Redis Error")
+        throw new APIError(404, "Url Not Found")
+    }
 
-
+    return res.status(201).json(new APIResponse(201, "Analytics Fetched Successfully", user.urlClicks))
 })
+
+
 
 export { urlAnalytics }
