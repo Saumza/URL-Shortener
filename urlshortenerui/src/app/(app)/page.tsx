67d7ef693a1be26616a3a1be0836765b1de8a09b
Saumza <saumyajeetbhowmick@gmail.com>
2026-06-17 03:19:56 +0530

    - Initialize URL shortener UI with Next.js, Tailwind CSS, and essential components - Implemented main layout and home page structure - Configured global styles with Tailwind CSS and custom themes

---
+++ urlshortenerui/src/app/(app)/page.tsx
@@ -0,0 +1,34 @@
+export default function Home() {
+  return (
+    <div className="items-center justify-center bg-black text-white">
+      <nav className='p-6 px-25'>
+        <div className='flex items-center justify-between'>
+          <a href='/' className='font-outfit text-4xl font-bold'>
+            Shortly
+          </a>
+          <div className='flex gap-10'>
+            <a href='#feature_section' className='font-outfit text-lg font-medium'>
+              Features
+            </a>
+            <a href="#faq_section" className='font-outfit text-lg font-medium'>
+              FAQ
+            </a>
+            <a href="/" className='font-outfit text-lg font-medium'>
+              Plans
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div className='flex flex-col border-black border-2 px-25 py-4 mt-6 items-center'>
+        <div className='border w-fit rounded-3xl p-2 px-8 font-outfit text-[14px]'>
+          FAST • SECURE • RELIABLE
+        </div>
+        <section className='flex flex-col items-center mt-8 gap-1.5'>
+          <h2 className='text-6xl font-outfit font-semibold tracking-wide text-secondary'>Shorten Links,</h2>
+          <h2 className='text-6xl font-outfit font-semibold tracking-wide text-secondary'>Elevate Sharings</h2>
+          <p className='font-outfit font-light mt-3 w-115 p-2 text-center text-gray-300 '>Transform long, cluttered URLs into clean and memorable links in just a click</p>
+        </section>
+      </div>
+    </div>
+  );
+}
